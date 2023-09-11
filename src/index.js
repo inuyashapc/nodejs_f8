a74cae89eeb77b7cafb1ad50db9a7321d3f64be1
Kyle - DA <inuyashapc162@gmail.com>
2023-09-11 10:59:28 +0700

    Refactor code: create routes, app/controllers folders

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const route = require("./routes");
+const db = require("./config/db");
+
+//Connect to db
+db.connect();
+
+//HTTP logger
+// app.use(morgan("combined"));
+
+//Template engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+//Routes init
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
